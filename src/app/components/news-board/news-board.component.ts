c835fafd3c09bef89e4df029efa37f5060957ac8
sk <sk.hyun@ingenuous.com.au>
2020-06-14 10:29:54 +0900

    added link of rss source link

--- src/app/components/news-board/news-board.component.ts
+++ src/app/components/news-board/news-board.component.ts
@@ -10,6 +10,7 @@ import { RSSFeed } from 'src/app/core/models/rssfeed';
 })
 export class NewsBoardComponent implements OnInit {
   newsRSS: RSSLink[];
+  curLink: RSSLink;
   /*
   [{label: 'reddit', link: 'https://www.reddit.com/r/worldnews/.rss'}, {label: 'BBC', link: 'http://feeds.bbci.co.uk/news/world/rss.xml'}
   ,{label: 'NewYork Times', link: 'https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/section/world/rss.xml'}, {label: 'Al Jazeera', link: 'https://www.aljazeera.com/xml/rss/all.xml'}
@@ -25,6 +26,7 @@ export class NewsBoardComponent implements OnInit {
   ngOnInit(): void {
     this.rssparser.getFeeds().subscribe(feeds => {
       this.newsRSS = feeds;
+      this.curLink = this.newsRSS[0];
       this.setNewsContent();
     });
   }
@@ -34,7 +36,7 @@ export class NewsBoardComponent implements OnInit {
     this.newsRSS.forEach(it => {
       try {
         this.rssparser.readRSSFeed(it).subscribe((dat: RSSFeed) => {
-          this.newsContent[it.label] = dat;
+          this.newsContent[it.id] = dat;
         });
       } catch(ex) {
         console.error(ex);
@@ -43,7 +45,8 @@ export class NewsBoardComponent implements OnInit {
   }
 
   fetchNews(evt) {
-    console.log('Test', evt);
+    console.log(evt);
+    this.curLink = this.newsRSS[evt.index];
   }
 
 }
