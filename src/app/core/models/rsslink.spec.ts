d5d52638c0e8773dd9a847bc4b6fac625bf849bc
sk.hyun <mysuccess@daum.net>
2021-05-26 15:02:38 +0900

    latest codes

--- src/app/core/models/rsslink.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RSSLink } from './rsslink';
-
-describe('RSSLink', () => {
-  it('should create an instance', () => {
-    expect(new RSSLink()).toBeTruthy();
-  });
-});
--- src/app/core/models/rsslink.ts
+++
@@ -1,7 +0,0 @@
-export class RSSLink {
-    id: number;
-    label: string;
-    link: string;
-    ttl?: number;
-    lastUpdated: string;
-} 
